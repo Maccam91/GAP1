cdae4b1ac0de0a433082ef92fce5a3409d2fe7f1
maccam91 <michael.cortezjr@gmail.com>
2022-01-09 18:59:17 -0600

    got my timing functions and all my correct screen elements working

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const playWords = ["red","blue","yellow","green","orange","violet","indigo","cat","dog","bird","quick","zebra","loop","Houston","Texas","713","832",
-"nine","money","league","challenge","slow","pink","complete","mvp","javaScript","periwinkle","lavender","magic","the","gathering","dungeon","dragon","esport","Aatrox","Rengar","Reddit","bang",
-"Pokemon","Haunter","Ash","wallet","steam","Epic","CAPS","LOCK","General","Assembly","axolotl","qi","zoolander","QWERTY","shift","enter","command","alt","control","rainbow","2+2","foodie"]
+"nine","money","league","challenge","slow","pink","complete","mvp","javaScript","periwinkle","lavender","magic","the","gathering","dungeon",
+"dragon","esport","Aatrox","Rengar","Reddit","bang",
+"Pokemon","Haunter","Ash","wallet","steam","Epic","CAPS","LOCK","General","Assembly","axolotl","qi","zoolander","QWERTY"
+,"shift","enter","command","alt","control","rainbow","2+2","foodie","computer","fish",
+"lizard","vapor","wave","Lo-Fi","Michael","SNER","rick","water","fire","air","earth","mutant","witcher","8675309",
+"rick","roll","never","gonna","give","you","up","turn","around","and","desert"]
 
 const playZone = document.getElementById('playArea')
 
@@ -11,66 +15,103 @@ music.volume = .025;
 music.playbackRate= 1;
 
 
-let round = 0;
-
-
+let roundTracker = 0
+let round = 0
+let roundTime = 0  
+let counter = document.getElementById('count').innerHTML
+     
 
 
 
 
 playZone.addEventListener('input', () => {
-  
-  // document.getElementById('bgmusic').play()
-  const arrayPrompt = promptDisplay.querySelectorAll('span')
-  const promptValue = playZone.value.split('')
-  let correct = true
-  arrayPrompt.forEach((characterSpan, index) => {
-    const character = promptValue[index]
-    if (character == null) {
-        characterSpan.classList.remove('correct')
-        characterSpan.classList.remove('incorrect')
-        correct = false
-      } else if (character === characterSpan.innerText) {
-        characterSpan.classList.add('correct')
-        characterSpan.classList.remove('incorrect')
-      } else {
-        characterSpan.classList.remove('correct')
-        characterSpan.classList.add('incorrect')
-        correct = false
-      }
-    })
-    if(correct) renderNewPrompt()  
-  
+
+roundTime = setInterval(() => { document.getElementById('count').innerText++  
+}, 1000);
+
+setTimeout(() =>{ document.getElementById('startScreen').style.display='none' } , 3000)
+
+document.getElementById('bgmusic').play();
+   
+const arrayPrompt = promptDisplay.querySelectorAll('span')
+
+const promptValue = playZone.value.split('')
+
+let correct = true
+
+arrayPrompt.forEach((characterSpan, index) => {
+
+const character = promptValue[index]
+    
+  if (character == null) {
+    characterSpan.classList.remove('correct')
+    characterSpan.classList.remove('incorrect')
+    correct = false 
+        
+    } else if (character === characterSpan.innerText) {
+      characterSpan.classList.add('correct')
+      characterSpan.classList.remove('incorrect')
+      clearTimeout(roundTracker)  
+      clearTimeout(roundTime)      
+    } else {
+      characterSpan.classList.remove('correct')
+      characterSpan.classList.add('incorrect')
+      correct = false    
+    }
+  })
+
+  if(correct){renderNewPrompt(); clearTimeout(roundTracker)} 
+
+  if(correct|false){roundTracker = setTimeout(() => { gameOver()} , 63000)}
+
 })
 
 async function renderNewPrompt() {
- const prompt = await newPrompt(round+1,playWords)
+
+const prompt = await newPrompt(round+1,playWords)
+
 promptDisplay.innerHTML = ''
+
 prompt.split('').forEach(character => {
+
   const characterSpan = document.createElement('span')
+
   characterSpan.innerText = character
+
   promptDisplay.appendChild(characterSpan)
+
 })
-playZone.value= null;
-roundsComplete()
+
+playZone.value = null;
+
+completedRound()
+
 }
 
 function newPrompt(length,x) {
+
     let result = [];
         
     for ( let i = 0; i < length; i++ ) {
-      const randomWords = (Math.floor(Math.random() * 
- playWords.length));
- result.push(x[randomWords])
-   }
-   return result.join(' ');
-   }
 
-  
+    const randomWords = (Math.floor(Math.random() * playWords.length)); result.push(x[randomWords])}
+    
+    return result.join(' ');
+
+   }  
      
-   function roundsComplete() {
+function completedRound() {
+
    round ++;
-   document.getElementById("round").innerHTML='Round   '+round;
+
+   document.getElementById("round").innerHTML='Round '+round;
+
    }
 
-// let endGame = setTimeout(())
+   function gameOver() {
+    
+    document.getElementById('gameOver').style.display ='block'
+    document.getElementById('gameOver').innerText ="Game Over you've reached"+'  '+document.getElementById('round').innerText+'  '+"Refresh to try again";
+    document.getElementById('bgmusic').pause();
+    
+  }
